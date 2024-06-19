4f34f996856c37024f3a0893c036fc3a7e6f1503
prajjwalz1 <acharyaprajjwol152@gmail.com>
2024-06-20 00:20:07 +0545

    remove venv from staging stage

--- venv/Lib/site-packages/tenant_users/tenants/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class TenantsConfig(AppConfig):
-    """Configuration for tenant_users.tenants app."""
-
-    default_auto_field = "django.db.models.AutoField"
-    name = "tenant_users.tenants"
-    label = "tenant_users_tenants"
