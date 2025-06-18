b35184966b810b5bede845a63cf78a01484556a4
Prajjwal Acharya <34410880+prajjwalz1@users.noreply.github.com>
2025-06-18 19:34:50 +0545

    fixed the cors

--- core/settings.py
+++ core/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
-    "https://kailashcms.carringmanagement.com.au"
+    "https://kailashcms.carringmanagement.com.au",
     "http://localhost:8000",        # if you run Django dev server on 8000
     "http://127.0.0.1",
     "http://127.0.0.1:8000",       # add ports you use locally
